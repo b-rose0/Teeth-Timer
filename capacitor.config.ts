6cb840be618ca1efeb0b7c1403537e32c72a35e4
Berenice Rose <berenice.rose0@gmail.com>
2023-02-23 09:50:03 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Teeth_Timer',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
